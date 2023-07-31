1083e92a601858f08dd7d37672911c295e101c2d
beatriz arantes <beatrizarantes.dev@gmail.com>
2023-07-31 17:47:45 -0300

    Meu Primeiro Projeto

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -1,1 +1,13 @@
-print("Hello,World!!")
+menu = '''
+========= Sistema Bancário =======
+[1]Conta Corrente \n[2]Conta Universitária \n[3]Sair '''
+print(menu)
+
+conta = str(input("Digite a opção da sua conta: "))
+
+menu_1 = ''' 
+====== Escolha a operação ======
+[1]Depósito \n[2]Saque \n[3]Extrato \n[4]Cancelar operação'''
+print(menu_1)
+
+operacao = str(input("Digite a opção que deseja: "))
