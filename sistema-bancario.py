019f0e82509a4276629d69c2671d674bb2a5949c
beatriz arantes <beatrizarantes.dev@gmail.com>
2023-08-03 14:18:57 -0300

    Meu Primeiro Projeto

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -18,15 +18,14 @@ while True:
         print(depositar)
         deposito = float(input("Deposite o valor que deseja: "))
         print(f"O valor de R${deposito:,.2f} foi depositado na sua Conta Corrente!")
-        break
+
     if conta == "2" and operacao == "1":
         depositar_1 = ''' ====== Depósito ======'''
         print(depositar_1)
         deposito_1 = float(input("Deposite o valor que deseja: "))
         if deposito_1 <= limite_dep_universitaria:
             print(f"O valor de R${deposito_1:,.2f} foi depositado na sua Conta Universitária!")
-            break
         else:
             print(f"O valor de R${deposito_1:,.2f} excedeu o limite de R${limite_dep_universitaria} da sua Conta "
                   f"Universitária! \nTente com outro valor.")
-            break
+    break
